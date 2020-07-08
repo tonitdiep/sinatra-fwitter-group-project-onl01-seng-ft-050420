61b23e89220e2ea5eb1a68477959d67ddf81aa2a
Toni  Diep <adiep123@gmail.com>
2020-07-08 05:29:55 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_050831) do
+ActiveRecord::Schema.define(version: 2020_07_08_052854) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
