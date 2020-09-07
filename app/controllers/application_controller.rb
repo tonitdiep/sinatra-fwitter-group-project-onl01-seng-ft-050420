dcec35d3037bcffe9c269ddec99d82dbae04aa65
Toni  Diep <adiep123@gmail.com>
2020-09-07 18:30:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 require './config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  get '/' do 
+    "Welcome to Fwitter"
+  end
+  get '/users/signup' do 
+    erb :'/users/signup'
+  end
+  post '/users/signup' do 
+    @user = User.create(username: params[:username], password: params[:password], email: params[:email])
+    redirect '/users/signup'
+  end
 end
