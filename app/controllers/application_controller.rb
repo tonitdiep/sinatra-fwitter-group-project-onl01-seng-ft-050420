8c70aba1124bba06665534e87692f91027656ac3
Toni  Diep <adiep123@gmail.com>
2020-09-11 17:52:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < Sinatra::Base
     end
     
     def current_user
- 
       	@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
   end
