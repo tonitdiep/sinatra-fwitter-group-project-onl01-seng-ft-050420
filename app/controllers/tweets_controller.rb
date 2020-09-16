bf1fa9ef07c7fe5d88672ea981e66f591ccbe3d0
Toni  Diep <adiep123@gmail.com>
2020-09-16 17:19:04 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,13 +21,31 @@ class TweetsController < ApplicationController
     redirect "/tweets/#{@tweet.id}"
   end
   
-  get '/tweets/:id' do
+  get '/tweets/:id' do #show action
     @tweet = Tweet.find_by_id(params[:id])
     erb :'/tweets/show_tweet'
   end
   
+  get '/tweets/:id/edit' do #load edit form
 
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/edit'
+  end
   
+  patch '/tweets/:id' do
+    if current_user && is_logged_in? == content.user_id
+      @tweet = Tweet.find_by_id(params[:id])
+      # @tweet.content = params[:content]
+      # @tweet.user_id = params[:user_id]
+      # @tweet.save
+      @tweet.update(content: params[:content], user_id: params[:user_id])
+       @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
+    
+      erb :'/tweets/show_tweet'
+    else
+      redirect "/tweets/#{@tweet.id}"
+    end
+  end
   
   
 end
